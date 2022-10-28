2bd49b1db5f58e5328064df8b9ae22a98cb4fa56
Xinyu Liu <xla324@sfu.ca>
2022-10-28 15:41:46 -0700

    Add the model part.

---
+++ app/src/main/java/ca/cmpt276/coopachievement/model/Game.java
@@ -0,0 +1,41 @@
+package ca.cmpt276.coopachievement.model;
+
+import java.util.ArrayList;
+
+public class Game {
+    private int numPlayers;
+    private int score;
+    private AchievementList achievementList; //Single Game's Achievement List
+
+    public Game(int numPlayers, int score) {
+        this.numPlayers = numPlayers;
+        this.score = score;
+    }
+
+
+    // All Setters
+    public void setNumPlayers(int numPlayers) {
+        this.numPlayers = numPlayers;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setAchievementList(AchievementList achievementList) {
+        this.achievementList = achievementList;
+    }
+
+    // All Getters
+    public int getNumPlayers() {
+        return numPlayers;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public AchievementList getAchievementList() {
+        return achievementList;
+    }
+}
