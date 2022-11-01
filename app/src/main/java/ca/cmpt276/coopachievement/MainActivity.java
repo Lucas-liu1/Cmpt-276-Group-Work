5026cecea64d7bfebeb7722ed68e48960c598c5c
JOSH0320 <113145189+JOSH0320@users.noreply.github.com>
2022-10-31 17:44:27 -0700

    init commit.

--- app/src/main/java/ca/cmpt276/coopachievement/MainActivity.java
+++ app/src/main/java/ca/cmpt276/coopachievement/MainActivity.java
@@ -2,19 +2,12 @@ package ca.cmpt276.coopachievement;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import ca.cmpt276.coopachievement.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
