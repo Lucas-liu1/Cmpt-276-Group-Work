62d0b355aa7958d38e19bcbb7c00fad3ecaaf604
Xinyu Liu <xla324@sfu.ca>
2022-11-13 21:12:22 -0800

    Add model methods

--- myapplication2/src/main/java/ca/cmpt276/myapplication2/model/Game.java
+++ myapplication2/src/main/java/ca/cmpt276/myapplication2/model/Game.java
@@ -1,5 +1,7 @@
 package ca.cmpt276.myapplication2.model;
 
+import java.util.ArrayList;
+
 /**
  * This class is for a single game.
  */
@@ -8,10 +10,36 @@ public class Game {
     private int numPlayers;
     private int score;
     private AchievementList achievementList; //Single Game's Achievement List
+    private ArrayList<Integer> scoresList;
+    private String difficulty;
 
+    //should be deleted
     public Game(int numPlayers, int score) {
         this.numPlayers = numPlayers;
         this.score = score;
+    }
+
+    public Game(int numPlayers, ArrayList<Integer> scoresList, String difficulty) {
+        this.numPlayers = numPlayers;
+        this.scoresList = scoresList;
+        this.difficulty = difficulty;
+        score = 0;
+
+        //calculate the total score they got(ignore difficulty)
+        for(int i=0; i<scoresList.size(); i++){
+            score += scoresList.get(i);
+        }
+
+        //According to the difficulty, calculate the true score
+        if(difficulty == "normal"){
+            score *= 1;
+        }
+        else if(difficulty == "easy"){
+            score *= 0.75;
+        }
+        else if(difficulty == "hard"){
+            score *= 1.25;
+        }
     }
 
     // All Setters
