32065b0e190531f9884c30328126e35307964726
Xinyu Liu <xla324@sfu.ca>
2022-11-21 17:36:46 -0800

    Add a new JUnit Test about AchievementList

--- myapplication2/src/main/java/ca/cmpt276/myapplication2/model/AchievementList.java
+++ myapplication2/src/main/java/ca/cmpt276/myapplication2/model/AchievementList.java
@@ -55,7 +55,7 @@ public class AchievementList {
             Achievement Master = new Achievement("Master", (poor_score + 5 * interval) * numPlayers, great_score * numPlayers, numPlayers);
             achievementsList.add(Master);
 
-            //Grandmaster (For each player: great_score ~ 100)
+            //Grandmaster (For each player: great_score ~ positive infinity)
             Achievement Grandmaster = new Achievement("Grandmaster", great_score * numPlayers, Integer.MAX_VALUE, numPlayers);
             achievementsList.add(Grandmaster);
         }
@@ -90,7 +90,7 @@ public class AchievementList {
             Achievement Helicopter = new Achievement("Helicopter", (poor_score + 5 * interval) * numPlayers, great_score * numPlayers, numPlayers);
             achievementsList.add(Helicopter);
 
-            //Grandmaster (For each player: great_score ~ 100)
+            //Grandmaster (For each player: great_score ~ positive infinity)
             Achievement Plane = new Achievement("Plane", great_score * numPlayers, Integer.MAX_VALUE, numPlayers);
             achievementsList.add(Plane);
         }
@@ -124,7 +124,7 @@ public class AchievementList {
             Achievement Bear = new Achievement("Bear", (poor_score + 5 * interval) * numPlayers, great_score * numPlayers, numPlayers);
             achievementsList.add(Bear);
 
-            //Grandmaster (For each player: great_score ~ 100)
+            //Grandmaster (For each player: great_score ~ positive infinity)
             Achievement Elephant = new Achievement("Elephant", great_score * numPlayers, Integer.MAX_VALUE, numPlayers);
             achievementsList.add(Elephant);
         }
